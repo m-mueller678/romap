eccf4457793d45e00548630988ce20bf8ed7dd56
m-mueller678 <marcus.mueller.678@gmail.com>
2024-12-18 13:54:19 +0100

    check readme

--- check.sh
+++ check.sh
@@ -5,4 +5,10 @@ cargo clippy --all-features --all-targets -- -D warnings
 cargo +nightly fmt --all --check
 cargo test --all-features
 
+README_PATH="Readme.md"
+if ! cargo readme | diff -u "$README_PATH" - > /dev/null; then
+    echo -e "\033[1;31mReadme files differ. Please regenerate Readme.md.\033[0m" >&2
+    exit 1
+fi
+
 echo ok
